2e49fb2eb577a2709665dee1e561676a612e2e55
alor <alor@antifork.org>
2002-12-23 21:32:36 +0000

    preliminary db functions

---
+++ include/lmap_db.h
@@ -0,0 +1,89 @@
+#ifndef LMAP_DB_H
+#define LMAP_DB_H
+
+/* bucket content */
+
+struct L2_info {
+   char mac_src[6];
+   char mac_dst[6];
+   char *ESSID;
+};
+
+struct L3_info {
+   u_int32  ip_src;
+   u_int32  ip_dst;
+   int proto;
+   int ttl;
+};
+
+struct L4_info {
+   u_int16 port_src;
+   u_int16 port_dst;
+   int proto;
+   char *fingerprint;
+};
+ 
+struct L5_info {
+   int type;
+   union {
+      struct dhcp {
+         u_int32  ip;
+         u_int32  gw;
+         u_int32  dns;
+      };
+   } un;
+};
+
+/* 
+ * database core elements 
+ */
+
+struct L5_entry {
+   u_int64 id;
+   struct L5_info  *inf;
+   struct L5_entry *next;
+};
+
+struct L4_entry {
+   u_int64 id;
+   struct L4_info  *inf;
+   struct L4_entry *next;
+   struct L5_entry *sap;
+};
+
+struct L3_entry {
+   u_int64 id;
+   struct L3_info  *inf;
+   struct L3_entry *next;
+   struct L4_entry *sap;
+};
+
+struct L2_entry {
+   u_int64 id;
+   struct L2_info  *inf;
+   struct L2_entry *next;
+   struct L3_entry *sap;
+};
+
+
+/* 
+ * the struct bucket is used by database in order to exchange messages
+ * between applications and the engine 
+ */
+ 
+struct bucket {
+   struct L2_info *L2;
+   struct L3_info *L3;
+   struct L4_info *L4;
+   struct L5_info *L5;
+};
+   
+ 
+/* extern prototypes */
+
+#endif
+
+/* EOF */
+
+// vim:ts=3:expandtab
+
---
+++ src/lmap_db.c
@@ -0,0 +1,68 @@
+/*
+    lmap -- database module
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+    $Header$
+*/
+
+#include <lmap.h>
+#include <lmap_db.h>
+
+/* protos */
+
+struct bucket * db_bucket_alloc(void);
+void bd_bucket_free(struct bucket *);
+
+/*******************************************/
+
+/*******************************************/
+
+/* 
+ * create and alloc space for a bucket used for exchanging messages 
+ */
+
+struct bucket * db_bucket_alloc(void)
+{
+   struct bucket *p;
+
+   p = (struct bucket *) calloc(1, sizeof(struct bucket));
+   ON_ERROR(p, NULL, "can't allocate bucket");
+
+   p->L2 = (struct L2_info *) calloc(1,sizeof(struct L2_info));
+   p->L3 = (struct L3_info *) calloc(1,sizeof(struct L3_info));
+   p->L4 = (struct L4_info *) calloc(1,sizeof(struct L4_info));
+   p->L5 = (struct L5_info *) calloc(1,sizeof(struct L5_info));
+
+   if ( p->L2 == NULL || p->L3 == NULL || p->L4 == NULL || p->L5 == NULL )
+      ERROR_MSG("can't allocate memory");
+      
+   return p;
+}
+
+/* 
+ * dealloc space of bucket, keeping the content allocated
+ */
+ 
+void bd_bucket_free(struct bucket *p)
+{
+   SAFE_FREE(p);
+}
+
+
+/* EOF */
+
+// vim:ts=3:expandtab
+ 
