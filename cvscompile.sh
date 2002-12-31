947a2978cffa13007883aaac61aca26cb1fe6c54
alor <alor@antifork.org>
2002-12-31 13:19:31 +0000

    Makefiles should not be in the cvs. use ./cvscompile.sh to compile from cvs

--- cvscompile.sh
+++ cvscompile.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-echo "cleaning up configure and aclocal.m4"
+echo "cleaning up config files"
 rm -f configure
 rm -f aclocal.m4
+rm -f Makefile.in Makefile
+rm -f */Makefile */Makefile.in
 
 echo "running aclocal..."
 aclocal
+echo "running autoheader..."
+autoheader
 echo "running autoconf..."
 autoconf
 echo "running automake..."
